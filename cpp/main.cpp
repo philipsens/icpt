d78ea6250c67c1bea28a368e12fbaa8210cdaa4e
Sergi Philipsen <s1108198@student.hsleiden.nl>
2022-06-13 13:38:03 +0200

    make awsd work correct way

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,8 +1,44 @@
+/**
+ Linux (POSIX) implementation of _kbhit().
+ Morgan McGuire, morgan@cs.brown.edu
+ */
+#include <cstdio>
+#include <sys/select.h>
+#include <termios.h>
+#include <asm-generic/ioctls.h>
+#include <sys/ioctl.h>
+
+int _kbhit() {
+    static const int STDIN = 0;
+    static bool initialized = false;
+
+    if (!initialized) {
+        // Use termios to turn off line buffering
+        termios term;
+        tcgetattr(STDIN, &term);
+        term.c_lflag &= ~ICANON;
+        tcsetattr(STDIN, TCSANOW, &term);
+        setbuf(stdin, NULL);
+        initialized = true;
+    }
+
+    int bytesWaiting;
+    ioctl(STDIN, FIONREAD, &bytesWaiting);
+    return bytesWaiting;
+}
+
 #include <iostream>
 #include <csignal>
-#include <thread>
-#include <stdatomic.h>
-#include <atomic>
+
+bool clockwise = false;
+
+void down();
+
+void up();
+
+void left();
+
+void right();
 
 enum Direction {
     Up = 'A',
@@ -11,18 +47,32 @@ enum Direction {
     Left = 'D'
 };
 
-std::atomic<bool> stop;
+char printchar = 'x';
+
+void checkNextLoop(void (*callback)()) {
+    if (_kbhit()) {
+        return;
+    }
+    callback();
+}
 
 void moveCursor(Direction direction, int number = 1) {
     printf("\033[%d%c", number, direction);
 }
 
 void reset() {
-    moveCursor(Direction::Left);
+    if (clockwise) {
+        moveCursor(Direction::Left, 2);
+        checkNextLoop(up);
+    } else {
+        moveCursor(Direction::Left);
+        checkNextLoop(down);
+    }
 }
 
-void printX() {
-    std::cout << "x";
+void printChar() {
+    std::string s(1, printchar);
+    std::cout << s;
 }
 
 void delay() {
@@ -34,8 +84,9 @@ void left() {
     for (int i = 0; i < 9; i++) {
         delay();
         moveCursor(Direction::Left, 2);
-        printX();
+        printChar();
     }
+    checkNextLoop(reset);
 }
 
 void up() {
@@ -43,46 +94,63 @@ void up() {
         delay();
         moveCursor(Direction::Left);
         moveCursor(Direction::Up);
-        printX();
+        printChar();
+    }
+    if (clockwise) {
+        checkNextLoop(right);
+    } else {
+        checkNextLoop(left);
     }
 }
 
 void right() {
     for (int i = 0; i < 9; i++) {
         delay();
-        printX();
+        printChar();
+    }
+    if (clockwise) {
+        checkNextLoop(down);
+    } else {
+        checkNextLoop(up);
     }
 }
 
 void down() {
-    std::cout << "x";
     for (int i = 0; i < 9; i++) {
         delay();
         moveCursor(Direction::Left);
         moveCursor(Direction::Down);
-        printX();
+        printChar();
     }
-}
-
-void catch_signal(int sig_num)
-{
-    std::cout <<"HEY! somebody cancelled me..."<< std::endl;
-    exit( 1 );
-}
-
-void loop() {
-    while (!stop) {
-        down();
-        right();
-        up();
-        left();
-        reset();
+    if (clockwise) {
+        checkNextLoop(left);
+    } else {
+        checkNextLoop(right);
     }
 }
 
 int main() {
-    std::thread thread(loop);
-    thread.detach();
-    std::cin.get();
-    stop = true;
+    for (;;) {
+        switch (printchar) {
+            case 'w':
+                up();
+                break;
+            case 'a':
+                clockwise = true;
+                left();
+                break;
+            case 's':
+                down();
+                break;
+            case 'd':
+                clockwise = false;
+                right();
+                break;
+            default:
+                printchar = 'x';
+                down();
+                break;
+        }
+        printchar = getchar();
+    }
 }
