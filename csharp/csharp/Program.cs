bdac801560909f8e8ae262020fe6d2f7f7d6a1df
Sergi Philipsen <s1108198@student.hsleiden.nl>
2022-06-17 15:07:57 +0200

    Refactor code to make it 'sharp'

--- csharp/csharp/Program.cs
+++ csharp/csharp/Program.cs
@@ -7,7 +7,7 @@ namespace csharp
     internal class Resultaat
     {
         public string Code { get; set; }
-        public string EC { get; set; }
+        public string Ec { get; set; }
 
         public string Cijfer { get; set; }
 
@@ -15,91 +15,123 @@ namespace csharp
         {
             get
             {
-                if (Cijfer.ToLower() == "o") return "11";
-                if (Cijfer.ToLower() == "v") return "12";
-                if (Cijfer.ToLower() == "v") return "13";
-                return Cijfer;
+                return Cijfer.ToLower() switch
+                {
+                    "o" => "11",
+                    "v" => "12",
+                    "g" => "13",
+                    _ => Cijfer
+                };
             }
-            set { Cijfer = value; }
+            set => Cijfer = value;
         }
     }
 
-    internal class Program
+    internal static class Program
     {
         public static void Main(string[] args)
         {
-            using (var reader =
-                   new StreamReader(@"/home/sergi/Projects/2122.icpt-p4-s1108198/csharp/csharp/cijfers.csv"))
+            var resultaten = GetResultaten();
+            var column = AskUserWhichColumnToSort();
+            var order = AskUserAscendingDescending();
+
+            resultaten = SortFields(resultaten, column, order);
+
+            ShowResultaten(resultaten);
+        }
+
+        private static List<Resultaat> SortFields(List<Resultaat> resultaten, int column, int order)
+        {
+            var functionIndex = (column - 1) * 2 + order - 1;
+            return SortFieldsFunctions[functionIndex](resultaten);
+        }
+
+        private static void ShowResultaten(List<Resultaat> resultaten)
+        {
+            foreach (var resultaat in resultaten)
             {
-                List<Resultaat> list = new List<Resultaat>();
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    var values = line.Split(',');
-                    Resultaat resultaat = new Resultaat()
-                    {
-                        Code = values[0],
-                        EC = values[1],
-                        Cijfer = values[2]
-                    };
-                    list.Add(resultaat);
-                }
-
-                // Ask user which column to sort by
-                Console.WriteLine("Welk veld wil je sorteren?");
-                Console.WriteLine("1. Code");
-                Console.WriteLine("2. EC");
-                Console.WriteLine("3. Cijfer");
-                Console.Write(">> ");
-                int column = Convert.ToInt32(Console.ReadLine());
-                // Ask user if ascending or descending
-                Console.WriteLine("Wilt u de sorteervolgorde oplopend of aflopend?");
-                Console.WriteLine("1. Oplopend");
-                Console.WriteLine("2. Aflopend");
-                Console.Write(">> ");
-                int order = Convert.ToInt32(Console.ReadLine());
-
-                // Sort list
-                list = sortFields[(column - 1) * 2 + order - 1](list);
-
-                foreach (var resultaat in list)
-                {
-                    Console.WriteLine(resultaat.Code + "\t" + resultaat.EC + "\t" + resultaat.Cijfer);
-                }
+                Console.WriteLine($"{resultaat.Code}\t{resultaat.Ec}\t{resultaat.Cijfer}");
             }
         }
 
-        static Func<List<Resultaat>, List<Resultaat>>[] sortFields =
+        private static int AskUserAscendingDescending()
+        {
+            Console.WriteLine("Wilt u de sorteervolgorde oplopend of aflopend?");
+            Console.WriteLine("1. Oplopend");
+            Console.WriteLine("2. Aflopend");
+            Console.Write(">> ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        private static int AskUserWhichColumnToSort()
+        {
+            Console.WriteLine("Welk veld wil je sorteren?");
+            Console.WriteLine("1. Code");
+            Console.WriteLine("2. EC");
+            Console.WriteLine("3. Cijfer");
+            Console.Write(">> ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        private static List<Resultaat> GetResultaten()
+        {
+            var resultaten = new List<Resultaat>();
+            using var reader =
+                new StreamReader(@"/home/sergi/Projects/2122.icpt-p4-s1108198/csharp/csharp/cijfers.csv");
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (line == null) continue;
+                var values = line.Split(',');
+                var resultaat = new Resultaat()
+                {
+                    Code = values[0],
+                    Ec = values[1],
+                    Cijfer = values[2]
+                };
+                resultaten.Add(resultaat);
+            }
+
+            return resultaten;
+        }
+
+        private static readonly Func<List<Resultaat>, List<Resultaat>>[] SortFieldsFunctions =
         {
             (list) =>
             {
-                list.Sort((x, y) => x.Code.CompareTo(y.Code));
+                list.Sort((x, y) =>
+                    string.Compare(x.Code, y.Code, StringComparison.Ordinal));
                 return list;
             },
             (list) =>
             {
-                list.Sort((x, y) => y.Code.CompareTo(x.Code));
+                list.Sort((x, y) =>
+                    string.Compare(y.Code, x.Code, StringComparison.Ordinal));
                 return list;
             },
 
             (list) =>
             {
-                list.Sort((x, y) => x.EC.CompareTo(y.EC));
+                list.Sort((x, y) =>
+                    string.Compare(x.Ec, y.Ec, StringComparison.Ordinal));
                 return list;
             },
             (list) =>
             {
-                list.Sort((x, y) => y.EC.CompareTo(x.EC));
+                list.Sort((x, y) =>
+                    string.Compare(y.Ec, x.Ec, StringComparison.Ordinal));
                 return list;
             },
             (list) =>
             {
-                list.Sort((x, y) => x.NepCijfer.CompareTo(y.NepCijfer));
+                list.Sort((x, y) =>
+                    string.Compare(x.NepCijfer, y.NepCijfer, StringComparison.Ordinal));
                 return list;
             },
             (list) =>
             {
-                list.Sort((x, y) => y.NepCijfer.CompareTo(x.NepCijfer));
+                list.Sort((x, y) =>
+                    string.Compare(y.NepCijfer, x.NepCijfer, StringComparison.Ordinal));
                 return list;
             },
         };
