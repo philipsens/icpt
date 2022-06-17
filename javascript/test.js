30b073f882da90bd7faf7e6a084a91efa3e20904
Sergi Philipsen <s1108198@student.hsleiden.nl>
2022-06-17 15:07:00 +0200

    Add deep/shallow copy example

--- javascript/test.js
+++ javascript/test.js
@@ -30,10 +30,13 @@ const style = {
             transform: [{rotate: "45deg"}],
         },
         transform: [{rotate: "-45deg"}],
+        span: {
+            color: 'hotpink',
+        }
     }
 };
 
-function applyViewport(style, device, orientation) {
+function applyViewport(device, orientation) {
     const DEVICES = ['phone', 'tablet', 'screen', 'monitor']
 
     let newStyle = {}
@@ -57,11 +60,46 @@ function applyViewport(style, device, orientation) {
     return newStyle
 }
 
+test('shallow copy', () => {
+    // Shallow copy
+    const result = applyViewport(style, 'phone', 'portrait')
+
+    // Originally red, after execution blue.
+    // Change original to green.
+    style.a.color = 'green';
+    expect(result.a.color).toBe('blue')
+    // True: The value of primitive a.color got copied. The value didn't get changed.
+
+    // Originally hotpink, after execution stays hotpink.
+    // Change the original to red.
+    style.div.span.color = 'red';
+    expect(result.div.span.color).toBe('red')
+    // True: The address object div.span got copied. The value got changed.
+});
+
+test('deep copy', () => {
+    // Deep copy
+    const result = JSON.parse(JSON.stringify(applyViewport(style, 'phone', 'portrait')));
+
+    // originally red, after execution blue
+    style.a.color = 'green';
+    expect(result.a.color).toBe('blue')
+
+    // originally hotpink, after execution stays hotpink
+    style.div.span.color = 'red';
+    expect(result.div.span.color).toBe('hotpink')
+});
+
+
+
 test('phone portrait', () => {
     const expectedResult = JSON.stringify({
         "button": {"width": 20, "height": 50},
         "a": {"background": "grey", "color": "blue"},
-        "div": {"background": "grey", "transform": [{"rotate": "45deg"}]}
+        "div": {
+            "background": "grey", "transform": [{"rotate": "45deg"}],
+            "span": {"color": "hotpink"}
+        }
     })
 
     const result = JSON.stringify(applyViewport(style, 'phone', 'portrait'))
@@ -74,7 +112,10 @@ test('phone landscape', () => {
     const expectedResult = JSON.stringify({
         "button": {"width": 20, "height": 50},
         "a": {"background": "grey", "color": "blue"},
-        "div": {"background": "grey", "transform": [{"rotate": "45deg"}]}
+        "div": {
+            "background": "grey", "transform": [{"rotate": "45deg"}],
+            "span": {"color": "hotpink"}
+        }
     })
 
     const result = JSON.stringify(applyViewport(style, 'phone', 'landscape'))
@@ -86,7 +127,10 @@ test('tablet portrait', () => {
     const expectedResult = JSON.stringify({
         "button": {"width": 40, "height": 50},
         "a": {"background": "grey", "color": "red"},
-        "div": {"background": "grey", "transform": [{"rotate": "-45deg"}]}
+        "div": {
+            "background": "grey", "transform": [{"rotate": "-45deg"}],
+            "span": {"color": "hotpink"}
+        }
     })
 
     const result = JSON.stringify(applyViewport(style, 'tablet', 'portrait'))
@@ -98,7 +142,10 @@ test('tablet landscape', () => {
     const expectedResult = JSON.stringify({
         "button": {"width": 60, "height": 50},
         "a": {"background": "grey", "color": "red"},
-        "div": {"background": "grey", "transform": [{"rotate": "-45deg"}]}
+        "div": {
+            "background": "grey", "transform": [{"rotate": "-45deg"}],
+            "span": {"color": "hotpink"}
+        }
     })
 
     const result = JSON.stringify(applyViewport(style, 'tablet', 'landscape'))
